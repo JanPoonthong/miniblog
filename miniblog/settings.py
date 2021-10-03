7652ee083093534a78b2526af649f97aef2f7251
Jan Poonthong <janpoonthong628@gmail.com>
2021-10-03 21:00:38 +0700

    Disabling Debug mode cause issue

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
